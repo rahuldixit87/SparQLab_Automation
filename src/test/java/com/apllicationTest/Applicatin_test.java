ebf9fe8fb3bc1566aec3f714e5edd58f4fb09289
rahuldixit87 <103820251+rahuldixit87@users.noreply.github.com>
2022-06-09 14:34:42 +0530

    Create Applicatin_test.java

---
+++ src/test/java/com/apllicationTest/Applicatin_test.java
@@ -0,0 +1,39 @@
+package com.apllicationTest;
+
+import org.testng.annotations.Test;
+
+import com.SpurQlabs.SparQlab;
+import com.SpurQlabs.WebDriverClass;
+
+public class Applicatin_test extends WebDriverClass {
+
+
+	@Test
+	public void AddtionTest () throws InterruptedException {
+		SparQlab pages  = SparQlab.getSparQlab();
+		//LaunchBrowser();
+		pages.addition ();Thread.sleep(2000);
+		//closeBrowser();
+		Thread.sleep(3000);
+
+	}
+
+	@Test
+	public void substractionTest () {
+		SparQlab pages  = SparQlab.getSparQlab();
+		//LaunchBrowser();
+		pages.substraction();
+		//closeBrowser();
+	}
+@Test
+	public void	divisionTest() throws InterruptedException {
+		SparQlab pages  = SparQlab.getSparQlab();
+		pages.division();
+	}
+@Test
+	public void	multiplicationTest() throws InterruptedException {
+		SparQlab pages  = SparQlab.getSparQlab();
+		pages.multiplication();
+	}
+}
+
