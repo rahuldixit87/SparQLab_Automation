8196a475c73dca8edde01c5dd03f4926736a0bd7
rahuldixit87 <103820251+rahuldixit87@users.noreply.github.com>
2022-06-09 14:34:37 +0530

    Create SparQlab.java

---
+++ src/main/java/com/SpurQlabs/SparQlab.java
@@ -0,0 +1,155 @@
+package com.SpurQlabs;
+
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+
+
+public class SparQlab  {
+	
+	public WebDriver driver = WebDriverClass.getDriver ();
+	// finding the elements  	
+
+ // find requred Webelements 
+	
+ @FindBy (xpath="//span[text()='AC']")
+private   WebElement Allclear ;  
+
+  @FindBy (xpath="//div[@id='sciOutPut']")
+ private   WebElement ansewr ;  
+  
+@FindBy (xpath="//div[@id='sciInPut']")
+ private   WebElement textbarinput ;  
+
+
+public void addition () {
+               
+          try {
+        	  Thread .sleep(3000);
+        	 
+		  Actions action = new Actions(driver); 
+		  action.sendKeys(Keys.SUBTRACT).build().perform();
+		  action.sendKeys(Keys.NUMPAD2).build().perform();
+		  action.sendKeys(Keys.NUMPAD3).build().perform();
+		  action.sendKeys(Keys.NUMPAD4).build().perform();
+		  action.sendKeys(Keys.NUMPAD2).build().perform();
+		  action.sendKeys(Keys.NUMPAD3).build().perform();
+		  action.sendKeys(Keys.NUMPAD4).build().perform();
+		  action.sendKeys(Keys.ADD).build().perform();
+		  action.sendKeys(Keys.NUMPAD3).build().perform();
+		  action.sendKeys(Keys.NUMPAD4).build().perform();
+		  action.sendKeys(Keys.NUMPAD5).build().perform();
+		  action.sendKeys(Keys.NUMPAD3).build().perform();
+		  action.sendKeys(Keys.NUMPAD4).build().perform();
+		  action.sendKeys(Keys.NUMPAD5).build().perform();
+		
+	Thread.sleep(3000);
+	        String numAns = ansewr.getText();
+	        String ans = "111111";
+		   System.out.println("addition  "+numAns);
+		   
+		  // Assert.assertEquals(ans,numAns);
+	
+          } 
+          catch (Exception e )
+          {
+        	 Assert.fail("addtion is not matching ");
+        	 System.out.println("Addition test case fail ");
+          }
+	 }
+
+public void substraction () {
+	try {
+		Allclear.click();
+	Thread .sleep(3000);
+	Allclear.click();
+	  Actions action = new Actions(driver); 
+	  action.sendKeys(Keys.SUBTRACT).build().perform();
+	  action.sendKeys(Keys.NUMPAD2).build().perform();
+	  action.sendKeys(Keys.NUMPAD3).build().perform();
+	  action.sendKeys(Keys.NUMPAD4).build().perform();
+	  action.sendKeys(Keys.NUMPAD8).build().perform();
+	  action.sendKeys(Keys.NUMPAD2).build().perform();
+	  action.sendKeys(Keys.NUMPAD3).build().perform();
+	  action.sendKeys(Keys.SUBTRACT).build().perform();
+	  action.sendKeys(Keys.NUMPAD2).build().perform();
+	  action.sendKeys(Keys.NUMPAD3).build().perform();
+	  action.sendKeys(Keys.NUMPAD0).build().perform();
+	  action.sendKeys(Keys.NUMPAD9).build().perform();
+	  action.sendKeys(Keys.NUMPAD4).build().perform();
+	  action.sendKeys(Keys.NUMPAD8).build().perform();
+	  action.sendKeys(Keys.NUMPAD2).build().perform();
+	  action.sendKeys(Keys.NUMPAD3).build().perform();
+	
+
+	   System.out.println("substraction    "+ansewr.getText());
+	  // Assert.assertEquals(ansewr.getText(), 111111);
+
+    } 
+    catch (Exception e )
+    {
+  	 Assert.fail("substraction is not matching ");
+  	 System.out.println("substraction test case fail ");
+    }
+}
+
+public void division  () throws InterruptedException{
+	try {
+	Allclear.click();
+	Thread .sleep(3000);
+	//Allclear.click();
+	  Actions action = new Actions(driver); 
+	 
+	  action.sendKeys(Keys.NUMPAD4).build().perform();
+	  action.sendKeys(Keys.NUMPAD0).build().perform();
+	  action.sendKeys(Keys.NUMPAD0).build().perform();
+	  action.sendKeys(Keys.NUMPAD0).build().perform();
+	  action.sendKeys(Keys.DIVIDE).build().perform();
+	  action.sendKeys(Keys.NUMPAD2).build().perform();
+	 
+	  action.sendKeys(Keys.NUMPAD0).build().perform();
+	  action.sendKeys(Keys.NUMPAD0).build().perform();
+	  System.out.println("division      "+ansewr.getText());
+} 
+catch (Exception e )
+{
+	 Assert.fail("division is not matching ");
+	 System.out.println("division test case fail ");
+} 
+	
+	
+}
+public void multiplication (){
+	try {
+		Allclear.click();
+		Thread .sleep(3000);
+		//Allclear.click();
+		  Actions action = new Actions(driver); 
+		 
+		  action.sendKeys(Keys.NUMPAD4).build().perform();
+		  action.sendKeys(Keys.NUMPAD2).build().perform();
+		  action.sendKeys(Keys.NUMPAD3).build().perform();
+		  action.sendKeys(Keys.MULTIPLY).build().perform();
+		  action.sendKeys(Keys.NUMPAD5).build().perform();
+		  action.sendKeys(Keys.NUMPAD2).build().perform();
+		 
+		  action.sendKeys(Keys.NUMPAD5).build().perform();
+		  System.out.println(" multiplication   "+ansewr.getText());
+	} 
+	catch (Exception e )
+	{
+		 Assert.fail("multiplication () is not matching ");
+		 System.out.println("multiplication () test case fail ");
+	} 
+		
+
+}
+public static SparQlab getSparQlab() {
+	return PageFactory.initElements(WebDriverClass.getDriver (), SparQlab.class);
+}
+}
+
